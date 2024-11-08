6569d3ed5b118d2be2cc6d4733a48fdbebd2b4bc
yosima050 <byosep050@gmail.com>
2024-11-08 08:35:23 +0700

    Percobaan Pertama

---
+++ BioskopMod226.java
@@ -0,0 +1,30 @@
+public class BioskopMod226 {
+    public static void main(String[] args) {
+        String[][] penonton = new  String[4][2];
+
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Chandra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
+        System.out.printf("%s\t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s\t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s\t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s\t %s\n", penonton[3][0], penonton[3][1]);
+
+        System.out.println(penonton.length);
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+            System.out.println("Penonton pada baris ke-3");
+
+            for (String i : penonton[2]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
